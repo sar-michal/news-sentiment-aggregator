29c181c6437f70b562a8da198faa91fa9f0ff1a2
Michał Sarosiek <sarmichal@yahoo.com>
2026-03-10 14:30:26 +0100

    Add a simple test for scraping

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+import json
 from app.services.gdelt import GdeltFetcher
+from app.services.scraper import NewsScraper
 
 app = FastAPI(
     title="News Sentiment Aggregator API",
@@ -10,7 +12,7 @@ app = FastAPI(
 async def root():
     return {"status": "ok", "message": "ok"}
 
-# --- TEST ROUTE ---
+# --- TEST ROUTES ---
 @app.get("/test-gdelt")
 async def test_gdelt():
     fetcher = GdeltFetcher()
@@ -20,3 +22,20 @@ async def test_gdelt():
         "count": len(articles),
         "data": articles
     }
+
+@app.get("/test-scrape")
+async def test_scrape():
+    with open("/app/app/test_articles.json", "r") as file:
+        test_articles = json.load(file)
+
+    if not test_articles:
+        return {"message": "No articles found"}
+    
+    scraper = NewsScraper()
+
+    with open("test_scrape2.txt", "a", encoding="utf-8") as output_file:
+        for item in test_articles["data"]:
+            url = item.get("url")
+            if url:
+                print(scraper.scrape_article(url), file=output_file)
+    return {"status": "ok"}
