0573cf86833a20353f4679f1945e63bb0d427e45
Michał Sarosiek <sarmichal@yahoo.com>
2026-03-22 09:11:38 +0100

    Optimize tasks and add elasticsearch storing Previous implementation initialized new instances of classes for each worker. Now, there is a single global instance that reduces the number of underlying connections.

--- backend/app/workers/tasks.py
+++ backend/app/workers/tasks.py
@@ -1,10 +1,33 @@
 import logging
 
+from celery.signals import worker_process_init
+from pydantic import ValidationError
+
+from app.schemas.article import ArticleData
+from app.services.es_client import ElasticClient
 from app.services.gdelt import GdeltFetcher
 from app.services.scraper import NewsScraper
 from app.workers.celery_app import celery
 
 logger = logging.getLogger(__name__)
+
+gdelt_fetcher = None
+scraper = None
+es_client = None
+
+
+@worker_process_init.connect
+def init_worker_services(**kwargs):
+    """
+    Celery Signal: Runs strictly once when the worker process boots up.
+    """
+    global gdelt_fetcher, scraper, es_client
+    logger.info("Initializing global worker services and connection pools...")
+    gdelt_fetcher = GdeltFetcher()
+    scraper = NewsScraper()
+    es_client = ElasticClient()
+
+    es_client.create_index()
 
 
 @celery.task(
@@ -17,8 +40,6 @@ logger = logging.getLogger(__name__)
 def trigger_gdelt_fetch(self):
     """Producer: Fetches the latest GDELT articles and queues respective scraping tasks."""
     logger.info("Starting GDELT fetch...")
-
-    gdelt_fetcher = GdeltFetcher()
     articles = gdelt_fetcher.fetch_latest_news(max_records=50)
 
     if not articles:
@@ -35,20 +56,27 @@ def trigger_gdelt_fetch(self):
 @celery.task(
     bind=True, max_retries=2, autoretry_for=(ConnectionError,), retry_backoff=30
 )
-def process_article(self, article_data: dict):
+def process_article(self, article_dict: dict):
     """Consumer: Downloads HTML and extracts article text."""
-    url = article_data.get("url")
+    try:
+        article_data = ArticleData(**article_dict)
+    except ValidationError as e:
+        logger.error(f"Failed to parse article data: {e}")
+        return "Failed: Invalid data schema"
+
+    url = str(article_data.url)
     logger.info(f"Processing article: {url}")
 
-    scraper = NewsScraper()
     text = scraper.scrape_article(url)
 
     if not text:
         logger.warning(f"Aborting processing for {url} - No text extracted.")
         return "Failed: No text"
 
-    # TODO: Add sentiment analysis
+    success = es_client.index_article(article_data, text)
+    if not success:
+        return "Failed: Elasticsearch indexing error"
 
-    # TODO: Add storing to Elasticsearch
+    # TODO: Add sentiment analysis
 
     return "Success"
