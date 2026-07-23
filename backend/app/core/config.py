e75c011de7ddf73f496c4a4043cefb88ca74215e
Michał Sarosiek <sarmichal@yahoo.com>
2026-07-23 16:14:22 +0200

    Add a missing comma in the url blacklist

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -81,7 +81,7 @@ class Settings(BaseSettings):
         "/commentisfree/",
         "/opinion/",
         "/opinions/",
-        "/editorials/"
+        "/editorials/",
         # 8. Administrative / Housekeeping
         "/corrections-and-clarifications",
     }
